62b143390b8dae93d8c4d0f59a123dbd29b402bb
shinyZu <shinysirichandra1141@gmail.com>
2022-02-15 19:35:38 +0530

    Updated content of Case_04-KITT

--- myprojects/Assignments/JavaScript/Case_04-KITT/assets/js/script.js
+++ myprojects/Assignments/JavaScript/Case_04-KITT/assets/js/script.js
@@ -6,125 +6,117 @@ let box5 = $("section:first-child > div > div:nth-child(5)");
 
 let scanner = [box1,box2,box3,box4,box5];
 
-let shade1 = "rgb(250, 0, 0)"; //red
-let shade2 = "rgb(0, 128, 0)"; //green
-let shade3 = "rgb(0, 0, 255)"; // blue
-let shade4 = 'rgb(255, 255, 0)'; // yellow
-let shade5 = "rgb(238, 130, 238)"; //pink
-let shade6 = "rgb(0, 255, 255)"; // aqua
+// let shade1 = "rgb(250, 0, 0)"; //red
+// let shade2 = "rgb(0, 128, 0)"; //green
+// let shade3 = "rgb(0, 0, 255)"; // blue
+// let shade4 = 'rgb(255, 255, 0)'; // yellow
+// let shade5 = "rgb(238, 130, 238)"; //pink
+// let shade6 = "rgb(0, 255, 255)"; // aqua
 
-// let shade1 = "rgb(192, 57, 43)";
-// let shade2 = 'rgb(231, 76, 60)';
-// let shade3 = "rgb(235, 77, 75)";
-// let shade4 = "rgb(255, 121, 121)";
-// let shade5 = "rgb(250, 177, 160)";
-// let shade6 = "rgb(255, 255, 255)";
+let shade1 = "rgb(192, 57, 43)";
+let shade2 = 'rgb(231, 76, 60)';
+let shade3 = "rgb(235, 77, 75)";
+let shade4 = "rgb(255, 121, 121)";
+let shade5 = "rgb(250, 177, 160)";
+let shade6 = "rgb(255, 255, 255)";
 
 let shades = [shade1,shade2,shade3,shade4,shade5];
 let initialShades = [shade6,shade6,shade6,shade6,shade6];
 
+let timerId = -1;
+let timerId1 = -1;
+let timerId2 = -1;
+// let timerId3;
+let timeOut = 100;
+
+
+// (function(){
+//     // timerId = setInterval(changeColor,timeOut);
+//     // timerId1 = setInterval(moveForward,timeOut);
+// })();
+
+function start() {  
+
+    timerId = setInterval(changeColor,timeOut);
+    timerId1 = setInterval(moveForward,timeOut);
+}
+
+function stop() {  
+}
+
+// stop();
+start();
+
 function changeColor(){
-    // console.log(1);
 
     let firstShade = shades.shift();
     initialShades.unshift(firstShade);
-    let x = initialShades.pop();
-    shades.push(x);
 
-    // console.log(shades);
-    // console.log(initialShades);
+    let lastShade = initialShades.pop();
+    shades.push(lastShade);
 }
 
-let i; 
-let j;
-let array = [];
-
 function moveForward() {  
-    // console.log("start of 1");
 
-    for (j = 0; j < scanner.length; j++) {
-        // console.log("");
-        // console.log("start of 2");
-        console.log("---j----"+j+"----:----"+initialShades[j]);
-
-        for (i in scanner) {
-            // console.log(i);
-            // console.log("start of 3");
-            console.log(i+"---:---"+scanner[i].text());
-            
-            scanner[i].css("background-color",initialShades[i]);
-
-            // console.log("end of 3");
-            
-            // scanner[0].css("height","100px");
-            // scanner[1].css("height","100px");
-        
-            // shades.forEach(k => {
-            //     // console.log(k);
-            //     if (scanner[4].css("background-color") == shade5) {
-            //         // console.log("equals...........");
-            //         scanner[4].css("background-color","black");
-            //         // changeShadesWithinBoxOne();
-            
-            //     } 
-            // });
-
-            // scanner.forEach( (k) => {
-            //     if (k.css("background-color") == shade6) {
-            //         console.log(true);
-            //         // k.css("background-color","black");
-            //     }
-            // });
-
-
-            // if (scanner[4].css(":focus") ) {
-            //     console.log("focused");
-            // }
-            
-
-            // if (j == 4 ) {
-            //     console.log(j);
-            // }
-            // scanner[4].css("background-color","black");
-
-            // j++;
-        }
-        // console.log("end of 2");
+    for (i in scanner) {
+        scanner[i].css("background-color",initialShades[i]);
     }
-    // console.log("end of 1");
-    
-    timerId2 = setInterval(moveBackwards,1000);
-    // clearInterval(timerId2);
-    // clearInterval(timerId1);
-    // start();
 
-    // for (let t = 11000; t > 0; t*10) {
-        
-    // }
+    timerId2 = setTimeout(moveBackwards,timeOut*10);
+    // timerId2 = setInterval(moveBackwards,t*10);
+    
 }
 
 function moveBackwards(){
+
+    clearInterval(timerId1);
+    timerId1 = setTimeout(moveForward,timeOut*10);
+    // timerId1 = setInterval(moveForward,timeOut*10);
+
     for (let i = scanner.length-1; i >= 0; i--) {
         scanner[i].css("background-color",initialShades[(scanner.length-1)-i]);
     }
 
-    clearInterval(timerId2);
-    timerId1 = setInterval(moveForward,1000);
+    clearTimeout(timerId2);
+    timerId1 = setInterval(moveForward,timeOut*10);
+        
 }
 
-let timerId1;
-let timerId2;
+$("#btnStop").click(function (e) { 
 
-// timerId2 = setInterval(moveBackwards,11000);
+    console.log(clearInterval(timerId));
 
+    clearInterval(timerId);
+    clearInterval(timerId1);
+    clearTimeout(timerId1);
+    clearTimeout(timerId2);
 
-(function(){
+    timerId = -1;
+    timerId1 = -1;
+    timerId2 = -1;
     
-    // setInterval(changeColor,100);
-    // clearInterval(timerId1);
-    // clearInterval(timerId2);
-    // timerId1 = setInterval(moveForward,100);
-    // clearInterval(timerId2);
- 
+    console.log(timerId); 
+    console.log(timerId1);
+    console.log(timerId2);
+});
 
-})();
+
+$("#btnStart").click(function (e) { 
+
+    // clearInterval(timerId);
+    // clearInterval(timerId1);
+    // clearTimeout(timerId1);
+    // clearTimeout(timerId2);
+
+    start();
+
+    // timerId = -1;
+    // timerId1 = -1;
+    // timerId2 = -1;
+
+    console.log(timerId);
+    console.log(timerId1);
+    console.log(timerId2);
+
+
+});
